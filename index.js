11c07100991ac86b390f48af37d4f1e230f77d2c
Tibor Végh <73848390+photovegh@users.noreply.github.com>
2021-11-14 22:08:14 +0100

    NodeJS Atlas MongoDB GET POST template. (userName, email, age, messaga)

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+path;
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(express.static("public/js"));
+app.use(express.static("public/css"));
+app.use(express.static("public/img"));
+app.set("port", process.env.PORT || 9876);
+
+/* ####### REQUEST / */
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+/* ####### REQUEST /ROUTING GET */
+app.get("/person", (req, res) => {
+    const client = getClient();
+    client.connect(async (err) => {
+        const collection = client.db("post-test").collection("test");
+        const person = await collection.find().toArray();
+        client.close();
+        res.send(person);
+    });
+});
+
+/* ####### REQUEST /ROUTING POST */
+app.post("/person", bodyParser.json(), (req, res) => {
+    const newPerson = {
+        userName: req.body.userName,
+        email: req.body.email,
+        age: req.body.age,
+        message: req.body.message,
+    };
+    const client = getClient();
+    client.connect(async (err) => {
+        const collection = client.db("post-test").collection("test");
+        const result = await collection.insertOne(newPerson);
+        if (result.insertedCount) {
+            res.send({ error: "nem lehet insertálni, azaz creálni" });
+            console.log(result.insertedCount);
+            return;
+        }
+    });
+    res.send(newPerson);
+    client.close();
+});
+
+/* ####### CONNECTION STRING */
+function getClient() {
+    const { MongoClient } = require("mongodb");
+    const uri =
+        "mongodb+srv://photovegh:Sususoft_0913@cluster0.gfjvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    return new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
+/* ####### APP LISTING */
+app.listen(app.get("port"), function () {
+    console.log(
+        "Az expressz elindult a http: // test:" +
+            app.get("port") +
+            " helyen; a Ctrl-C megnyomásával zárja be a szervert."
+    );
+});
